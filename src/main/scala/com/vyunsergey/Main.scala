5f8b1ea488bc67acb5aa8c7d1c108adec13f4771
Vyun Sergey <vyun.sergey@icloud.com>
2020-06-19 10:20:58 +0300

    new logic for checkResult: brand "Эконика" new logic for checkRules: compute statistics new logic for createClusters: new census data

--- src/main/scala/com/vyunsergey/Main.scala
+++ src/main/scala/com/vyunsergey/Main.scala
@@ -4,7 +4,9 @@ import com.vyunsergey.args._
 import com.vyunsergey.conf._
 import com.vyunsergey.spark._
 import com.vyunsergey.struct._
+import org.apache.spark.sql.expressions.Window
 import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
 import zio._
 import zio.console._
 
@@ -17,9 +19,9 @@ object Main extends App {
    * Main program function
    */
   def run(args: List[String]): ZIO[ZEnv, Nothing, Int] = {
-//    createClusters(args)
-//    createBrands(args)
-    checkResult(args)
+    createClusters(args)
+//    checkRules(args)
+//    checkResult(args)
       .provideSomeLayer[ZEnv](Configuration.live ++ SparkEnv.live)
       .foldM(
         error => putStrLn(s"Execution failed with error: $error") *> ZIO.succeed(1),
@@ -52,7 +54,7 @@ object Main extends App {
     // filter on Column mcc_supercat
     superCat    = "Всё для дома"
     // filter Rules from Excel with Column mcc_supercat
-    rules      <- SparkApp.filterData(excel, "mcc_supercat", Some(superCat)).provide(spark)
+    rules      <- SparkApp.filterData(excel, "mcc_supercat", Some(superCat)).provide(spark).map(_.cache)
     // create Brands Expression as String
     expr       <- SparkApp.createBrandExpression(rules, "brand", "reg_exp").provide(spark)
     _          <- putStrLn(s"expression:\n${SparkApp.BothSubstring(expr, 1000)}")
@@ -61,14 +63,13 @@ object Main extends App {
     _          <- putStrLn(s"count: ${data.count}")
     // create Brands from Census cluster DataFrame with Brands Expression
     udf        <- Task(udf((x: mutable.WrappedArray[String]) => x.length))
-    brands     <- SparkApp.withBrandColumn(data, "brand", rules,
-      "brand", "reg_exp").provide(spark)
+    brands     <- SparkApp.withBrandColumn(data, "brand", rules, "brand", "reg_exp").provide(spark)
                    .map(_.filter(col("brand").isNull)
                          .withColumn("has_brand", col("brand").isNotNull)
                          .withColumn("mcc_clr", substring(SparkApp.clearColumn(col("mcc_name")), 0, 30))
                          .withColumn("merchant_clr", substring(SparkApp.clearColumn(col("merchant_nm")), 0, 30))
                          .withColumn("words", udf(split(col("merchant_clr"), " ")))
-                         .cache())
+                         .cache)
     _          <- putStrLn(s"count: ${brands.count}")
     _          <- putStrLn(s"schema: ${brands.printSchema()}")
     _          <- putStrLn(s"sample: ${brands.filter(col("brand").isNull).show(50, truncate = false)}")
@@ -77,52 +78,137 @@ object Main extends App {
 //    checkGroup <- SparkApp.countGroupBy(checkBrand, "mcc_supercat", "merchant_clr", "merchant_id", "merchant_city_nm").provide(spark)
 //    _          <- putStrLn(s"stats: ${checkGroup.show(300, truncate = false)}")
     // collect Statistics with Brands on Census cluster DataFrame
-    brandsStat <- SparkApp.countStatistics(brands, "mcc_supercat", "brands_part", "has_brand").provide(spark)
+    brandsStat <- SparkApp.countStatistics(brands, "mcc_supercat", "brands_part", "merchant_city_nm", "has_brand").provide(spark)
     _          <- putStrLn(s"stats: ${brandsStat.show(50, truncate = false)}")
-    brandsGrp  <- SparkApp.countGroupBy(brands, "mcc_supercat", "brands_part", "has_brand").provide(spark)
+    brandsGrp  <- SparkApp.countGroupBy(brands, "mcc_supercat", "brands_part", "merchant_city_nm", "has_brand").provide(spark)
                   //.map(_.filter(col("count") > 100)
                   //      .orderBy(col("words").asc, col("count").desc)
                   //)
     _          <- putStrLn(s"stats: ${brandsGrp.show(50, truncate = false)}")
-    _          <- SparkApp.writeData(conf.writeConf, brands, writePath, List("brands_part")).provide(spark)
+    brandsMsc  <- Task(brands
+                       .withColumn("brand", when(col("brand") === "НЕ БРЕНД", "")
+                         .when(col("brand") === "ИП", "").otherwise(col("brand")))
+                       .withColumn("brand", coalesce(col("brand"), lit("")))
+                       .withColumn("merchant_nm", coalesce(col("merchant_nm"), lit("")))
+                       .withColumn("terminal_id", coalesce(col("terminal_id"), lit("")))
+                       .filter(col("merchant_city_nm").isin("MOSCOW", "MOSKVA", "G MOSKVA", "MASKVA"))
+                       .filter(col("latitude_mt") =!= "\\N" && col("latitude_mt").isNotNull)
+                       .filter(col("longitude_mt") =!= "\\N" && col("longitude_mt").isNotNull)
+                       .filter(col("brand").isin("Эконика"))
+                       .withColumn("rn", row_number().over(Window.partitionBy(col("merchant_nm"),
+                         col("terminal_id"), col("latitude_mt"), col("longitude_mt"))
+                         .orderBy(col("brand").desc_nulls_last, col("terminal_id").desc_nulls_last)))
+                       .filter(col("rn") === 1)
+                       .drop(col("rn"))
+                       .withColumn("brand_count", sum(lit(1L)).over(Window.partitionBy(col("brand"))))
+                       .withColumn("brand", when(col("brand") === "", "Остальное").otherwise(col("brand")))
+                       .withColumn("rank", dense_rank().over(Window.orderBy(col("brand_count").desc, col("brand"))))
+                       .withColumn("latitude_mt", col("latitude_mt").cast(DecimalType(18, 6)))
+                       .withColumn("longitude_mt", col("longitude_mt").cast(DecimalType(18, 6)))
+                       .cache)
+    _          <- putStrLn(s"brandsMsc count: ${brandsMsc.count}")
+    _          <- putStrLn(s"brandsMsc sample: ${brandsMsc.show(50, truncate = false)}")
+    mapMoscow  <- Task(brandsMsc
+                       .filter(col("brand_count") > 10)
+                       .filter(col("latitude_mt") >= 55.55 && col("latitude_mt") <= 55.95)
+                       .filter(col("longitude_mt") >= 37.35 && col("longitude_mt") <= 37.85)
+                       .groupBy(col("latitude_mt"), col("longitude_mt"), col("brand"))
+                       .agg(
+                         max(col("merchant_nm")).as("merchant_nm"),
+                         max(col("terminal_id")).as("terminal_id"),
+                         max(col("brand_count")).as("brand_count"),
+                         max(col("rank")).as("rank")
+                       )
+                       .select(
+                         col("latitude_mt").as("Широта"),
+                         col("longitude_mt").as("Долгота"),
+                         concat_ws(" ", col("brand"), col("merchant_nm"),
+                           col("terminal_id")).as("Описание"),
+                         col("brand").as("Подпись"),
+                         col("rank").as("Номер метки"),
+                         col("brand_count"))
+                       .orderBy(col("Номер метки").desc))
+    _          <- putStrLn(s"mapMoscow count: ${mapMoscow.count}")
+    _          <- putStrLn(s"mapMoscow sample: ${mapMoscow.show(50, truncate = false)}")
+    _          <- SparkApp.writeData(conf.writeConf, mapMoscow
+                  .select("Широта", "Долгота", "Описание", "Подпись", "Номер метки"), writePath).provide(spark)
     _          <- Task(spark.stop())
   } yield ()
 
   /*
    * Check Statistics on Result DataFrame cluster`s from Source BigData Census DataFrame
    */
-  def createBrands(args: List[String]): ZIO[AppEnvironment, Throwable, Unit] = for {
+  def checkRules(args: List[String]): ZIO[AppEnvironment, Throwable, Unit] = for {
     conf       <- config
     args       <- ArgumentsParser.parse(args)
     readPath    = args.flatMap(_.readPath)
     writePath   = args.flatMap(_.writePath)
     _          <- putStrLn(s"read path: $readPath")
     _          <- putStrLn(s"write path: $writePath")
-    spark      <- createSparkSession("census-brands", 8)
-    data       <- SparkApp.readData(conf.readConf, CensusSchema.schema, readPath).provide(spark)
-    travel     <- SparkApp.filterData(data, "brands_part", Some("travel_4")).provide(spark)
-    hotel      <- Task(travel.filter("trim(mcc_name) like 'Отели – %'"))
-    _          <- putStrLn(s"count: ${hotel.count}")
-    _          <- putStrLn(s"schema: ${hotel.printSchema()}")
-    _          <- putStrLn(s"sample: ${hotel.show(false)}")
-    result     <- Task(
-                   hotel
-                     .withColumn("brand",
-                       expr("regexp_replace(trim(mcc_name), 'Отели – ', '')"))
-                     .withColumn("mcl",
-                       expr("trim(lower(brand))"))
-                     .withColumn("rule", expr("concat_ws(''," +
-                       "'mcc_code IN (\\''," +
-                       "mcc_code," +
-                       "'\\') AND trim(lower(mcc_name)) regexp \\''," +
-                       "mcl," +
-                       "'\\'')"))
-    )
-    statsBrand <- SparkApp.countStatistics(result
-                                            .filter("length(brand) >= 3")
-                                            .filter("brand not in('неиспользуемый код', 'другие компании')"),
-      "mcc_name", "rule", "brand").provide(spark)
-    _          <- putStrLn(s"stats: ${statsBrand.show(300, truncate = false)}")
+    spark      <- createSparkSession("census-brands", 16)
+    superCat    = "Дети и животные"
+    text       <- SparkApp.readAsText(conf.readConf, readPath).provide(spark)
+    _          <- putStrLn(s"count: ${text.count}")
+    _          <- putStrLn(s"schema: ${text.printSchema}")
+    _          <- putStrLn(s"sample: ${text.show(false)}")
+    rdd        <- SparkApp.splitLineRDD(text.rdd, conf.readConf.fileConf.separator, CensusSchema.schema.length).provide(spark)
+    data       <- SparkApp.createDataFrame(rdd, CensusSchema.schema).provide(spark)
+    _          <- putStrLn(s"count: ${data.count}")
+    _          <- putStrLn(s"schema: ${data.printSchema()}")
+    _          <- putStrLn(s"partitions: ${data.rdd.getNumPartitions}")
+    _          <- putStrLn(s"sample: ${data.show(false)}")
+    rules      <- SparkApp.readExcel(conf.readConf, BrandsDictSchema.schema).provide(spark)
+                  .map(_.filter(col("mcc_supercat") === superCat)
+                        .cache)
+    expr       <- SparkApp.createBrandExpression(rules, "brand", "reg_exp").provide(spark)
+    _          <- putStrLn(s"expression:\n${SparkApp.BothSubstring(expr, 1000)}")
+    udf        <- Task(udf((x: mutable.WrappedArray[String]) => x.length))
+    brands     <- SparkApp.withBrandColumn(data, "brand", rules, "brand", "reg_exp").provide(spark)
+                  .map(_.filter(col("mcc_supercat") === superCat)
+                        .withColumn("area_mt", when(col("area_mt") === "\\N", null)
+                          .otherwise(col("area_mt")))
+                        .withColumn("locality_mt", when(col("locality_mt") === "\\N", null)
+                          .otherwise(col("locality_mt")))
+                        .withColumn("street_mt", when(col("street_mt") === "\\N", null)
+                          .otherwise(col("street_mt")))
+                        .withColumn("house_mt", when(col("house_mt") === "\\N", null)
+                          .otherwise(col("house_mt")))
+                        .withColumn("longitude_mt", when(col("longitude_mt") === "\\N", null)
+                          .otherwise(col("longitude_mt")))
+                        .withColumn("latitude_mt", when(col("latitude_mt") === "\\N", null)
+                          .otherwise(col("latitude_mt")))
+                        .withColumn("has_brand", col("brand").isNotNull)
+                        .withColumn("has_address", col("area_mt").isNotNull || col("locality_mt").isNotNull ||
+                          col("street_mt").isNotNull || col("house_mt").isNotNull)
+                        .withColumn("has_crd", col("longitude_mt").isNotNull && col("latitude_mt").isNotNull)
+                        .withColumn("mcc_clr", substring(SparkApp.clearColumn(col("mcc_name")), 0, 30))
+                        .withColumn("merchant_clr", substring(SparkApp.clearColumn(col("merchant_nm")), 0, 30))
+                        .withColumn("words", udf(split(col("merchant_clr"), " ")))
+                        .withColumn("all_cnt", sum(lit(1L)).over(Window.partitionBy(lit(1L))))
+                        .cache)
+    _          <- putStrLn(s"count: ${brands.count}")
+    _          <- putStrLn(s"schema: ${brands.printSchema}")
+    _          <- putStrLn(s"partitions: ${brands.rdd.getNumPartitions}")
+    _          <- putStrLn(s"sample: ${brands.show(100, truncate = false)}")
+    brandsGrp  <- Task(brands
+                       .groupBy(col("mcc_supercat"))
+                       .agg(
+                         max(col("all_cnt")).cast(DecimalType(24, 6)).as("all_cnt"),
+                         sum(when(col("has_brand"), 1L).otherwise(0L)).cast(DecimalType(24, 6)).as("brand_cnt"),
+                         sum(when(col("has_address"), 1L).otherwise(0L)).cast(DecimalType(24, 6)).as("address_cnt"),
+                         sum(when(col("has_crd"), 1L).otherwise(0L)).cast(DecimalType(24, 6)).as("crd_cnt"))
+                       .select(
+                         col("mcc_supercat"),
+                         col("all_cnt").cast(IntegerType).as("all_cnt"),
+                         col("brand_cnt").cast(IntegerType).as("brand_cnt"),
+                         col("address_cnt").cast(IntegerType).as("address_cnt"),
+                         col("crd_cnt").cast(IntegerType).as("crd_cnt"),
+                         ((col("brand_cnt") / col("all_cnt")) * 100.0).cast(DecimalType(24, 2)).as("brand_prc"),
+                         ((col("address_cnt") / col("all_cnt")) * 100.0).cast(DecimalType(24, 2)).as("address_prc"),
+                         ((col("crd_cnt") / col("all_cnt")) * 100.0).cast(DecimalType(24, 2)).as("crd_prc"))
+                       .cache)
+    _          <- putStrLn(s"stats count: ${brandsGrp.count}")
+    _          <- putStrLn(s"stats sample: ${brandsGrp.show(10, truncate = false)}")
     _          <- Task(spark.stop())
   } yield ()
 
@@ -145,8 +231,8 @@ object Main extends App {
     first      <- SparkApp.getFirst(text).provide(spark)
     _          <- putStrLn(s"first: ${first.mkString}")
     text2      <- SparkApp.filterRow(text, first).provide(spark)
-    rdd        <- SparkApp.splitLineRDD(text2.rdd, conf.readConf.fileConf.separator).provide(spark)
-    data       <- SparkApp.createDataFrame(rdd, CensusSchema.schema).provide(spark)
+    rdd        <- SparkApp.splitLineRDD(text2.rdd, conf.readConf.fileConf.separator, CensusBrandSchema.schema.length).provide(spark)
+    data       <- SparkApp.createDataFrame(rdd, CensusBrandSchema.schema).provide(spark)
     _          <- putStrLn(s"count: ${data.count}")
     _          <- putStrLn(s"schema: ${data.printSchema()}")
     _          <- putStrLn(s"partitions: ${data.rdd.getNumPartitions}")
